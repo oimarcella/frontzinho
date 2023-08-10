3eab872e124684f8f84edb30affe2b757af8e6ae
mlasa <marcella.amorimsa@gmail.com>
2023-08-10 09:42:08 -0300

    style: login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,7 @@ import { Container, Form, Col, Row } from "react-bootstrap";
 import TopBarComponent from "../../components/layout/components/topbar/topbar";
 import Button from "../../components/layout/components/button/button";
 import Footer from "../../components/layout/components/footer/footer";
-import { ContentStyled } from "./styles";
+import { ContentStyled, PageStyled, WrapperOverflow } from "./styles";
 import HeaderPage from "../../components/layout/components/headerPage/headerPage";
 import { useLocation } from "react-router-dom";
 import { ERoutes } from "../../core/enums/routes";
@@ -11,39 +11,42 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Content = () => {
     return (
-        <ContentStyled>
-            <Row className="d-flex align-items-center justify-content-center" style={{ height: "100%" }}>
-                <Col lg="6">
-                    <HeaderPage
-                        textToStyle="login"
-                        style={{ color: "#FF41AD" }}
-                        title="Olá, seja bem vindo(a)!"
-                        text="Faça seu login"
-                    />
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Control type="email" placeholder="jon.doe@myemail.com" />
-                            <Form.Text className="text-muted">
-                                Nós nunca compartilharemos esse endereço de e-mail com ninguém.
-                            </Form.Text>
-                        </Form.Group>
+        <PageStyled>
+            <WrapperOverflow>
+                <ContentStyled>
+                    <Row className="d-flex align-items-center justify-content-center" style={{ height: "100%" }}>
+                        <Col lg="5">
+                            <div className="formWrapper">
+                                <HeaderPage
+                                    textToStyle="login"
+                                    style={{ color: "#FF41AD" }}
+                                    title="Olá, seja bem vindo(a)!"
+                                    text="Faça seu login"
+                                />
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Control type="email" placeholder="jon.doe@myemail.com" />
+                                    </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Control type="password" placeholder="Senha" />
-                        </Form.Group>
-                        {/*<Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>*/}
-                        <div className="d-flex flex-column align-items-center">
-                            <Button color="#FF41AD" outlined="none" customStyles={{ width: '100%' }}>
-                                Entrar
-                            </Button>
-                            <a href={ERoutes.SIGNUP} className="mt-3 d-flex align-items-center">Criar conta <FontAwesomeIcon className="ms-1" icon={faArrowRight} /></a>
-                        </div>
-                    </Form>
-                </Col>
-            </Row>
-        </ContentStyled>
+                                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                                        <Form.Control type="password" placeholder="Senha" />
+                                    </Form.Group>
+                                    {/*<Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                        <Form.Check type="checkbox" label="Check me out" />
+                                    </Form.Group>*/}
+                                    <div className="d-flex flex-column align-items-center">
+                                        <Button color="#FF41AD" outlined="none" customStyles={{ width: '100%' }}>
+                                            Entrar
+                                        </Button>
+                                        <a href={ERoutes.SIGNUP} className="mt-3 d-flex align-items-center">Criar conta <FontAwesomeIcon className="ms-1" icon={faArrowRight} /></a>
+                                    </div>
+                                </Form>
+                            </div>
+                        </Col>
+                    </Row>
+                </ContentStyled>
+            </WrapperOverflow>
+        </PageStyled >
     );
 }
 
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,13 +1,32 @@
 import { Container } from "react-bootstrap";
 import styled from "styled-components";
 
+export const PageStyled = styled.div`
+    background-image: url('src/assets/images/petfoot.jpg');
+    background-position:  top;
+    background-repeat: no-repeat;
+    background-size: cover;
+`;
+
 export const ContentStyled = styled(Container)`
     padding-top: 50px;
     padding-bottom: 50px;
+    min-height: 80vh;
+
+    .formWrapper{
+        background: white;
+        padding: 2rem;
+        border-radius: 18px;
+    }
 
     @media (max-width: 1000px) {
-        padding-top: 40px;
-        padding-bottom: 40px;
+        //padding-top: 40px;
+        //padding-bottom: 40px;
         min-height: unset;
     }
+`;
+
+export const WrapperOverflow = styled.div`
+    background: rgb(231,242,248);
+    background: linear-gradient(180deg, rgba(210,231,242,0.6) 4%, rgba(205,228,241,0.6) 4%);
 `;
